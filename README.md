8259a31a0d80a43bf4e67a6fd5f4cd346e190f77
Vasiliy Vanchuk (@vvscode) <v.vanchuk@tut.by>
2019-01-21 21:33:26 +0400

    Count most frequent wordsa

--- README.md
+++ README.md
@@ -1,7 +1,28 @@
 # Frequency Analysis of Words
 
-[TODO. There will be project description]
+Скрипт принимает  на вход путь до текстового файла и выводит в консоль десять самых популярных слов в этом файле в порядке убывания частоты.
 
 # Project Goals
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
+
+## How to use
+
+`python3 lang_frequence.py file_name.txt`
+
+Пример:
+
+```bash
+$ python3 lang_frequency.py lang_frequency.py
+Top 10 most frequent words from file lang_frequency.py:
+print
+import
+words
+filename
+most_frequest_words
+file
+re
+from
+counter
+sys
+```
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,31 @@
+import re
+from collections import Counter
+import sys
 
+number_of_printed_words = 10
 
 def load_data(filepath):
-    pass
+    with open(filepath) as filepointer:
+        return filepointer.read()
 
 
 def get_most_frequent_words(text):
-    pass
-
+    words = re.findall(r"\w+", text.lower())
+    words_counter = Counter(words)
+    return words_counter.most_common(number_of_printed_words)
 
 if __name__ == '__main__':
-    pass
+    try:
+        filename = sys.argv[1]
+        file_content = load_data(filename)
+        most_frequest_words = get_most_frequent_words(file_content)
+        number_of_words = len(most_frequest_words)
+        print("Top {} most frequent words from file {}:".format(
+            number_of_words,
+            filename
+        ))
+        print("\n".join(x[0] for x in most_frequest_words))
+    except IndexError:
+        print("Please pass file name as a param")
+    except FileNotFoundError:
+        print("Please use correct file name")

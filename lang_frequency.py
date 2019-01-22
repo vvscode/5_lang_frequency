1f3f4f2190595dd40216da0d0239856eff0308f0
Vasiliy Vanchuk (@vvscode) <v.vanchuk@tut.by>
2019-01-22 10:23:35 +0400

    Fix notices

--- lang_frequency.py
+++ lang_frequency.py
@@ -2,32 +2,33 @@ import re
 from collections import Counter
 import sys
 
-number_of_printed_words = 10
-
 
 def load_data(filepath):
     with open(filepath) as filepointer:
         return filepointer.read()
 
 
-def get_most_frequent_words(text):
+def get_most_frequent_words(text, number_of_printed_words=5):
     words = re.findall(r"\w+", text.lower())
     words_counter = Counter(words)
     return words_counter.most_common(number_of_printed_words)
 
 
 if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        sys.exit("Please pass file name as a param")
+
+    filename = sys.argv[1]
     try:
-        filename = sys.argv[1]
         file_content = load_data(filename)
-        most_frequest_words = get_most_frequent_words(file_content)
-        number_of_words = len(most_frequest_words)
-        print("Top {} most frequent words from file {}:".format(
-            number_of_words,
-            filename
-        ))
-        print("\n".join(x[0] for x in most_frequest_words))
-    except IndexError:
-        print("Please pass file name as a param")
     except FileNotFoundError:
-        print("Please use correct file name")
+        sys.exit("Please use correct file name")
+
+    most_frequest_words = get_most_frequent_words(
+        file_content, number_of_printed_words=10)
+    number_of_words = len(most_frequest_words)
+    print("Top {} most frequent words from file {}:".format(
+        number_of_words,
+        filename
+    ))
+    print("\n".join(x[0] for x in most_frequest_words))
